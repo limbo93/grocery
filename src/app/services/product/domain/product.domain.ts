dfe87dd42cf5f2ae9706fdaca7d786f723182a0b
limbo93 <mhriad0@gmail.com>
2019-06-06 13:04:22 +0600

    product code added

--- src/app/services/product/domain/product.domain.ts
+++ src/app/services/product/domain/product.domain.ts
@@ -3,6 +3,7 @@ import { BaseDomain } from '../../base.domain';
 export class Product extends BaseDomain {
     id: number;
     name: string;
+    code: string;
     description: string;
     buyingPrice: number;
     sellingPrice: number;
